eb050cfd467ac4ff4000941a54cd4a49559dc954
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-08 16:24:27 -0700

    Second Commit

--- src/main/java/country/Country.java
+++ src/main/java/country/Country.java
@@ -3,5 +3,7 @@ package country;
 public class Country {
     public static void main(String[] args) {
         System.out.println("Country is India");
+        System.out.println("India is Rich in Heritage and Prosperity");
+        System.out.println("India is great");
     }
 }
